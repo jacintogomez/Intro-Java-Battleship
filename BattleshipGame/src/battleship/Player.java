093e548ef71a58eedd03527afb81e03b94895f53
Jacinto Gomez <63368475+jacintogomez@users.noreply.github.com>
2023-05-09 00:58:23 -0400

    Add files via upload

--- BattleshipGame/src/battleship/Player.java
+++ BattleshipGame/src/battleship/Player.java
@@ -1,145 +1,114 @@
-package battleship;
+package PartIV;
 
-import java.awt.BorderLayout;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.net.Socket;
-import java.net.UnknownHostException;
+import java.util.*;
 
-import javax.swing.*;
+import javax.swing.JFrame;
 
-public class Player extends JFrame implements Runnable {
+public class Player{
+	String username;
+	String password;
+	int id;
+	int numberofwins;
 	
-	private static final long serialVersionUID = 1L;
-	private static int WIDTH = 400;
-	private static int HEIGHT = 300;
-	private JTextArea ta;
-	private JTextField tf;
-	DataOutputStream toServer;
-	DataInputStream fromServer;
-	private Socket socket;
-	private static int numPlayers = 0;
-
-	
-	public Player() throws IOException {
-		super("Player");
-		this.setSize(Player.WIDTH, Player.HEIGHT);
-		this.setLayout(new BorderLayout());
-		createMenu();
-		ta = new JTextArea();
-		JScrollPane sp = new JScrollPane(ta);
-		this.add(sp, BorderLayout.CENTER);
-		tf = new JTextField();
-		tf.addKeyListener(new TextFieldListener());
-		this.add(tf, BorderLayout.SOUTH);
+	public Player(String username,String password,int id) {
+		this(username,password,id,0);
 	}
 	
-	private void createMenu() {
-		JMenuBar menuBar = new JMenuBar();
-		JMenu menu = new JMenu("File");
-		JMenuItem exitItem = new JMenuItem("Exit");
-		exitItem.addActionListener((e) -> {
-			System.exit(0);
-		});
-		JMenuItem connectItem = new JMenuItem("Connect");
-		connectItem.addActionListener((e) -> {
-			Thread t = new Thread(this);
-			t.start();
-		});
-		menu.add(connectItem);
-		menu.add(exitItem);
-		menuBar.add(menu);
-		this.setJMenuBar(menuBar);
+	public Player(String username,String password,int id,int numberofwins) {
+		this.username=username;
+		this.password=password;
+		this.id=id;
+		this.numberofwins=numberofwins;
 	}
-
-	class TextFieldListener implements KeyListener {
-
-		@Override
-		public void keyPressed(KeyEvent e) {
-			if(e.getKeyCode() == KeyEvent.VK_ENTER) {
-				try {
-					  
-					toServer = new DataOutputStream(socket.getOutputStream());
 	
-				} catch (IOException e1) {
-					closeEverything(socket, toServer, fromServer);
-					e1.printStackTrace();
+	public static int[][] flip(int grid[][]){
+		for(int x=0;x<10;x++) {
+			for(int y=0;y<x;y++) {
+				if(x!=y) {
+					int temp=grid[x][y];
+					grid[x][y]=grid[y][x];
+					grid[y][x]=temp;
 				}
-			    try {
-			    	String name = tf.getText().trim();
-		            toServer.writeUTF(name);
-			        toServer.flush();
-			        ta.append(name + "\n");
-			        tf.setText("");
-	
-			      }
-			      catch (IOException ex) {
-			        System.err.println(ex);
-			        closeEverything(socket, toServer, fromServer);
-			      } catch (NumberFormatException nfe) {
-			    	  System.err.println(nfe);
-			    	  ta.append("Bad format!\n");
-			      }
 			}
 		}
-
-		@Override
-		public void keyTyped(KeyEvent e) {
-		}
-
-		@Override
-		public void keyReleased(KeyEvent e) {
-		}		
-	  }
-		
-	@Override
-	public void run() {
-			try {
-				socket = new Socket("localhost", 8000);
-				ta.append("connected\n");
-				fromServer = new DataInputStream(socket.getInputStream());
-			} catch (UnknownHostException e1) {
-				e1.printStackTrace();
-			} catch (IOException e1) {
-				e1.printStackTrace();
-			}
-			
-			while(true) {
-				String clientInfo = null;
-				try {
-					clientInfo = fromServer.readUTF();
-					ta.append(clientInfo + "\n");
-				} catch (IOException e) {
-					closeEverything(socket, toServer, fromServer);
-					e.printStackTrace();
-				} 
-			}
+		return grid;
 	}
 	
-	public void closeEverything(Socket socket, DataOutputStream outputStream, DataInputStream inputStream) {
-		  try {
-			  if(inputStream != null) {
-				  inputStream.close();
-			  }
-			  if(outputStream != null) {
-				  outputStream.close();
-			  }
-			  if(socket != null) {
-				  socket.close();
-			  }
-		  }catch(IOException ex) {
-      		ex.printStackTrace();
-      	}
-	  }
-
-	public static void main(String[] args) throws IOException{
-		Player battleshipPlayer = new Player();
-		battleshipPlayer.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		battleshipPlayer.setVisible(true);
-
+	public static void main(String[] args) {
+//		int myhitsleft=0,ophitsleft=0;
+//		int l=10;
+//		String username="Jacinto Rigal";
+//		String password="jg777";
+//		int opgrid[][]= {
+//				{2,2,2,1,1,1,1,1,1,1},
+//				{1,2,2,1,1,1,1,1,1,1},
+//				{1,1,1,1,1,1,1,3,1,1},
+//				{1,1,1,2,1,1,1,1,1,1},
+//				{1,1,1,2,1,1,1,1,1,1},
+//				{1,1,1,2,1,1,1,1,1,1},
+//				{3,1,1,1,1,1,1,1,1,1},
+//				{1,1,1,1,4,4,4,2,1,1},
+//				{1,3,1,1,1,1,1,3,3,1},
+//				{1,1,1,2,2,2,2,2,1,1},
+//		};
+//		int mygrid[][]={
+//				{1,1,1,1,1,1,1,1,1,1},
+//				{1,1,1,1,1,4,1,1,2,1},
+//				{1,1,1,1,1,2,1,1,2,1},
+//				{1,1,1,1,1,1,1,1,2,1},
+//				{1,2,2,2,1,1,1,1,2,1},
+//				{2,1,1,1,1,1,1,1,2,1},
+//				{4,1,1,1,1,4,2,2,2,1},
+//				{4,1,1,3,1,1,1,1,1,3},
+//				{1,1,1,1,3,1,1,1,3,1},
+//				{1,1,1,1,1,1,3,1,1,1},
+//		};
+//		mygrid=flip(mygrid);
+//		opgrid=flip(opgrid);
+//		ArrayList<Ship> myships=new ArrayList<Ship>();
+//		ArrayList<Ship> opships=new ArrayList<Ship>();
+//		ArrayList<Coordinate> cords=new ArrayList<Coordinate>();
+//		Coordinate c1=new Coordinate(7,4);
+//		Coordinate c2=new Coordinate(7,5);
+//		Coordinate c3=new Coordinate(7,6);
+//		Coordinate c4=new Coordinate(7,7);
+//		cords.add(c1);
+//		cords.add(c2);
+//		cords.add(c3);
+//		cords.add(c4);
+//		Ship destroyer=new Ship("Destroyer",3);
+//		Ship submarine=new Ship("Submarine",3);
+//		Ship patrol=new Ship("Patrol Boat",2);
+//		Ship battleship=new Ship("Battleship",4);
+//		Ship carrier=new Ship("Carrier",5);
+//		Ship enemy_destroyer=new Ship("Destroyer",3);
+//		Ship enemy_submarine=new Ship("Submarine",3);
+//		Ship enemy_patrol=new Ship("Patrol Boat",2);
+//		Ship enemy_battleship=new Ship("Battleship",4,3,true);
+//		Ship enemy_carrier=new Ship("Carrier",5);
+//		enemy_battleship.coords=cords;
+//		myships.add(destroyer);
+//		myships.add(submarine);
+//		myships.add(patrol);
+//		myships.add(battleship);
+//		myships.add(carrier);
+//		opships.add(enemy_destroyer);
+//		opships.add(enemy_submarine);
+//		opships.add(enemy_patrol);
+//		opships.add(enemy_battleship);
+//		opships.add(enemy_carrier);
+//		
+//		for(int x=0;x<l;x++) {
+//			for(int y=0;y<l;y++) {
+//				if(mygrid[x][y]==4) {myhitsleft++;};
+//				if(opgrid[x][y]==4) {ophitsleft++;};
+//			}
+//		}
+//		Board game=new Board(username,password,mygrid,opgrid,myships,opships,myhitsleft,ophitsleft);
+		Board game=new Board("jack","jill");
+		game.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    game.setVisible(true);    
+	    game.setResizable(true);
 	}
-
+	
 }
